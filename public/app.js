35815f9e3d907321f5e4d5c9c99ba98000f8d4f3
User <user@Users-MBP-7.home>
2016-02-02 22:36:21 +0000

    got to stop the bordering countries being added

--- public/app.js
+++ public/app.js
@@ -14,22 +14,45 @@ var loadCountries = function(){
 
 var selectedName = function(){
 
+  localStorage.currentCountry = {}
   var name = document.getElementById("s1").value
   var result = _.find(countries, function(o){return o.name === name;})
+  localStorage.setItem('currentCountry', JSON.stringify(result) );
+  countryInfo();
   
+  
+}
+
+var countryInfo = function(){
+
+  var result = JSON.parse(localStorage.getItem('currentCountry') ) 
+
   var countryTitle = document.querySelector(".country-name");
   countryTitle.innerText = result.name;
 
   var countryPopulation = document.querySelector(".population");
-  countryPopulation.innerText = "Population: " + result.population;
-
+  countryPopulation.innerText = "Population: " + Number(result.population).toLocaleString();
+  
   var countryCapital = document.querySelector(".capital");
   countryCapital.innerText = "Capital City: " + result.capital;
 
   var countryCurrency = document.querySelector(".currency");
   countryCurrency.innerText = "Currency: " + result.currencies;
-  
-  
+
+  borderArray = result.borders;
+
+  for(var i = 0; i < borderArray.length;  i++){
+    var borderer = _.find(countries, function(o){return o.alpha3Code === borderArray[i];});
+    //console.log(borderer)
+    var bordering = document.createElement("p")
+    bordering.style.display = "inline-block";
+    bordering.style.padding = "10px"
+    bordering.innerText = borderer.name;
+    bordering.style.color ="red"
+
+    var borderParent = document.querySelector(".borders");
+    borderParent.appendChild(bordering);
+  }  
 }
 
 
@@ -45,9 +68,9 @@ window.onload = function(){
       console.log('got the data')
       //console.log(request.responseText)
       countries = JSON.parse(request.responseText );
-      console.log(countries)
      
       loadCountries();
+      countryInfo();
       selected = document.getElementById("s1")
       selected.onchange = selectedName;
     }
